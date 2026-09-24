f63df3797aa95558216bdc0ad1fcdd71d244dba4
neveslee777-cell <neveslee777@gmail.com>
2026-09-25 00:17:36 +0800

    Fix keyAlias type in signingConfigs

--- simple_live_tv_app/android/app/build.gradle.kts
+++ simple_live_tv_app/android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String？
             keyPassword = keystoreProperties["keyPassword"] as String
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as String
